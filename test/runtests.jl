ce7cc8327c847046ff44cb9070e494795d048e39
Gyoshi <bakafish.com@gmail.com>
2025-01-26 14:50:03 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,13 @@
 using TrivialNumbers
 using Test
 
-const x = (1+2╱)
-const y = (2+3╱)
-const z = Trivial(2,-2)
-
 @testset "TrivialNumbers.jl" begin
+    ╱ = TrivialNumbers.╱
+    ╲ = TrivialNumbers.╲
+    x = (1+2╱)
+    y = (2+3╱)
+    z = Trivial(2,-2)
     @test TrivialNumbers.conj(1+2TrivialNumbers.╱) == 1+2TrivialNumbers.╲
     @test TrivialNumbers.verso(Trivial(0, 2, 3)) == Trivial(3, 0, 2)
-    @test -(x,y,z) == -(dual(x,y,z)...)
+    @test -(x,y,z) == -(TrivialNumbers.dual(x,y,z)...)
 end
